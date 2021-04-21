a5266b935aa19fb8f19d7fbc830f58a3aceb1a8e
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-21 16:39:41 +0200

    add numeri 1-100

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ var numeriErratiUtente = []
 
 while(numeriRandom.length < 5){
 
-    var i = Math.floor(Math.random() * 10) + 1;
+    var i = Math.floor(Math.random() * 100) + 1;
 
     if(numeriRandom.indexOf(i) === -1) numeriRandom.push(i);
 }
+
 alert("Simon says:  " + numeriRandom);
