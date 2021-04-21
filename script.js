e66a356247fa913af46662ddff6858672d9a1ea1
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-21 16:35:09 +0200

    add alert

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var numeriRandom = []
 var numeriCorrettiUtente = []
 var numeriErratiUtente = []
+
+
+while(numeriRandom.length < 5){
+
+    var i = Math.floor(Math.random() * 10) + 1;
+
+    if(numeriRandom.indexOf(i) === -1) numeriRandom.push(i);
+}
+alert("Simon says:  " + numeriRandom);
